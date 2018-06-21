ed1bf74cd1c7072194f82b370453573d110fbf1f
sbiggs <scott.biggs@consensus-builder.com>
2018-06-21 00:13:52 -0500

    Changed the font of the displays. More on fonts to come!

---
+++ app/src/main/java/sleepfuriously/com/biggsstopwatch2/MyDigitalFontTextView.java
@@ -0,0 +1,25 @@
+package sleepfuriously.com.biggsstopwatch2;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.support.v7.widget.AppCompatTextView;
+
+
+/**
+ * Use this class instead of TextView to display the old calculate
+ * looking font.
+ */
+public class MyDigitalFontTextView extends AppCompatTextView {
+
+//    private static final Typeface mTypeface = new Typeface();
+
+
+    public MyDigitalFontTextView (Context ctx, AttributeSet attrs) {
+        super(ctx, attrs);
+//        setTypeface();
+    }
+
+
+
+}
